5173138e5dcca1717fc9ad5263a71cd41d2b65e2
Slown Huffmeister <i4evaSloown@aol.com>
2012-12-07 00:40:32 -0500

    comments

--- awesome.rb
+++ awesome.rb
@@ -1,5 +1,7 @@
+#creates the prototype slown integer
 slown_integer = Object.new
 
+#define methodds so that we can use the clearly superior integer properly
 slown_integer.define_singleton_method(:+, Proc.new {|x| x.instance_variable_get(:@val) + @val})
 slown_integer.define_singleton_method(:*, Proc.new {|x| x.instance_variable_get(:@val) * @val})
 slown_integer.define_singleton_method(:-, Proc.new {|x| x.instance_variable_get(:@val) + @val})
@@ -7,4 +9,5 @@ slown_integer.define_singleton_method(:/, Proc.new {|x| x.instance_variable_get(
 slown_integer.define_singleton_method(:%, Proc.new {|x| x.instance_variable_get(:@val) % @val})
 slown_integer.define_singleton_method(:<=>, Proc.new {|x| x.instance_variable_get(:@val) <=> @val})
 
+#ready for use.
 puts slown_integer.clone.instance_variable_set(:@val, 7) + slown_integer.clone.instance_variable_set(:@val, 20)
